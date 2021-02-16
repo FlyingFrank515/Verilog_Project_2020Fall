6e3ed72624073ab8e3f19042cfe6abb085e5c1ee
FrankPan <a1687193@gmail.com>
2021-02-16 09:52:32 +0800

    add ign

--- HW2/library/addIoFiller_tpz.cmd
+++
@@ -1,8 +0,0 @@
-addIoFiller -cell PFEED20Z    -prefix IOFILLER
-addIoFiller -cell PFEED10Z    -prefix IOFILLER
-addIoFiller -cell PFEED5Z     -prefix IOFILLER
-addIoFiller -cell PFEED1Z     -prefix IOFILLER
-addIoFiller -cell PFEED0_1Z   -prefix IOFILLER
-addIoFiller -cell PFEED0_01Z  -prefix IOFILLER
-addIoFiller -cell PFEED0_005Z -prefix IOFILLER -fillAnyGap
-redraw
